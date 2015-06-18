715c8fe81313122ecdd4c3a62413b91702767217
Ethan Arterberry <ethanarterberry@gmail.com>
2015-06-17 18:47:06 -0700

    add php

---
+++ php/fuckyou.php
@@ -0,0 +1,10 @@
+<?php
+      $knowsSwear = true;
+      $isAngry = false;
+      if($isAngry == true && $knowsSwear == true) {
+        print "fuck you";
+      } else {
+      	print "i am okay, please carry on";
+      }
+      # output: i am okay, please carry on
+?>
